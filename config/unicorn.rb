a53969ac7d91da7b5c6cd646d81d78cae2220b10
saludsoporte <salud.aplicaciones@gmail.com>
2021-12-06 09:14:07 -0600

    configuraciones de soporte

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/home/soporte/soporte/sistema_soporte"
-pid "/home/soporte/soporte/sistema_soporte/tmp/pids/unicorn.pid"
-stderr_path "/home/soporte/soporte/sistema_soporte/log/unicorn.log"
-stdout_path "/home/soporte/soporte/sistema_soporte/log/unicorn.log"
+working_directory "/home/ruby3/soporte/sistema_soporte"
+pid "/home/ruby3/soporte/sistema_soporte/tmp/pids/unicorn.pid"
+stderr_path "/home/ruby3/soporte/sistema_soporte/log/unicorn.log"
+stdout_path "/home/ruby3/soporte/sistema_soporte/log/unicorn.log"
 
 listen "/tmp/unicorn.sistema_soporte.sock", :backlog=>64
 #listen "0.0.0.0:8443", :tcp_nopush => true
